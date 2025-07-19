8b2a0d09ce64eea2f467af352e5968b95e46ae73
Sakib <sakibul706@gmail.com>
2025-07-19 23:04:41 +0600

    build

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 set -ex
 
+# -ldflags="-X 'main.compiledDate=$(date +%s)' -X 'main.gitCommit=$(git rev-parse --short HEAD)'" -tags 'static'
 GOARCH=arm64 GOOS=linux CGO_ENABLED=0 go build -tags static -ldflags "-s -w" -o build/

2e688fa285b0dcec4a73467e847daf82b3f2e1b2
Sakib <sakibul706@gmail.com>
2026-01-11 15:16:26 +0600

    now if the index should follow the duplicate words original idex

--- dicts_extra.go
+++ dicts_extra.go
@@ -2,34 +2,54 @@ package main
 
 import (
 	"net/http"
+	"slices"
 	"strconv"
 	"strings"
 )
 
 func (s *dictConf) getQueries(w http.ResponseWriter, r *http.Request, curr string) (string, *TmplData) {
-	in := strings.TrimSpace(r.FormValue("w"))
-	queries := []string{}
-	if curr == hanswehrName {
-		// keeping the english search featuere availabe
-		queries = parseQuery(in, rmHarakats)
-	} else {
-		queries = parseQuery(in, rmNonAr)
-	}
-
 	t := TmplData{Curr: curr, Dicts: dicts, DictsMap: dictsMap}
 
-	if len(queries) == 0 {
+	in := strings.TrimSpace(r.FormValue("w"))
+	queries := strings.Split(in, " ")
+
+	curQuery := ""
+	idx, err := strconv.Atoi(r.FormValue("idx"))
+	if err == nil && idx > -1 && idx < len(queries) {
+		t.Idx = idx
+		curQuery = queries[idx]
+	}
+
+	cleanQuries := []string{}
+	cleanCurrQuery := ""
+	if curr == hanswehrName {
+		// keeping the english search featuere availabe
+		cleanQuries = parseQuery(in, rmHarakats)
+		cleanCurrQuery = rmHarakats(curQuery)
+	} else {
+		cleanQuries = parseQuery(in, rmNonAr)
+		cleanCurrQuery = rmNonAr(curQuery)
+	}
+
+	if len(cleanQuries) == 0 {
 		le(s.t.ExecuteTemplate(w, mainTemplateName, &t))
 		return "", nil
 	}
 
-	t.Query = strings.Join(queries, " ")
-	t.Queries = queries
-	idx, err := strconv.Atoi(r.FormValue("idx"))
-	if err == nil && idx > -1 && idx < len(queries) {
-		t.Idx = idx
-	} else {
-		t.Idx = len(queries) - 1
+	if len(cleanQuries) != len(queries) {
+		if cleanCurrQuery != "" {
+			if i := slices.Index(cleanQuries, cleanCurrQuery); i > -1 {
+				t.Idx = i
+			} else {
+				// should no happend just in case
+				t.Idx = len(cleanQuries) - 1
+			}
+		} else {
+			t.Idx = len(cleanQuries) - 1
+		}
 	}
-	return strings.ReplaceAll(queries[t.Idx], "_", " "), &t
+
+	t.Query = strings.Join(cleanQuries, " ")
+	t.Queries = cleanQuries
+	return strings.ReplaceAll(cleanQuries[t.Idx], "_", " "), &t
 }
